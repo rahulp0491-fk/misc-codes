079d5caf2405a026f578cabe54f21e000284885c
Rahul P <rahulp0491@gmail.com>
2014-08-31 12:15:40 +0530

    fixed syntax error

--- algos/string-matching/sm_common.h
+++ algos/string-matching/sm_common.h
@@ -10,6 +10,6 @@ using namespace std;
 void naive_string_matcher(string, string);
 bool naive_string_matcher_for_pattern_with_gaps(string, string);
 void rabin_karp_matcher(string, string, uint64_t, uint64_t);
-void rkm_for_k_patterns_of_same_len(string, vector <string>, uint32_t, uint64_t, uint64_t)
+void rkm_for_k_patterns_of_same_len(string, vector <string>, uint32_t, uint64_t, uint64_t);
 
 #endif
