be1f7e68dc238297b16f536f42c5f50c9a2f855e
Rahul P <rahulp0491@gmail.com>
2014-08-31 16:52:09 +0530

    added graph init

--- algos/graph/applications.cpp
+++ algos/graph/applications.cpp
@@ -53,6 +53,12 @@ void check_bipartite(int n) {
   printf ("[%s] bipartite graph\n", __FUNCTION__);
 }
 
+void graph_init() {
+  memset(vis, 0, sizeof(vis));
+  memset(pi, -1, sizeof(pi));
+  memset(color, 0, sizeof(color));
+}
+
 int main() {
   int n, m, u, v;
   scanf("%d %d", &n, &m);
@@ -62,6 +68,8 @@ int main() {
     adj[v].push_back(u);
   }
   
+  graph_init();
+
   /* Bipartite check */
   check_bipartite(n);
   return 0;
